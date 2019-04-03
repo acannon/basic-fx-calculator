76778d1a7768c40e405aedfa591774e905944299
Alexandra Cannon <cannonalexandra@gmail.com>
2019-04-02 20:28:30 -0700

    Fixed "single digit only" issue. Implemented stack for complex operations.

--- src/core/Controller.java
+++ src/core/Controller.java
@@ -5,22 +5,28 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
+import java.util.EmptyStackException;
+import java.util.Stack;
+
 public class Controller {
 
     @FXML
     private Label result;
-    private float a = 0;
-    private float b = 0;
-    private String op = "";
     private boolean start = true;
-    private boolean firstNum = true;    // "are we looking for the first number?"
+    String lastKey = "";
+
 
     private Model model = new Model();
 
     @FXML
+    public void clear() {
+        start = true;
+        result.setText("");
+    }
+
+    @FXML
     public void processNumbers(ActionEvent e) {
-        // if we just opened the calculator, set display to blank
-        if(start) {
+        if(start || lastKey.equals("=")) {
             result.setText("");
             start = false;
         }
@@ -28,57 +34,43 @@ public class Controller {
         // get the value that was pressed by getting the Button label
         String value = ((Button)e.getSource()).getText();
 
-        // if this is the first number, assign to a
-        if(firstNum == true) {
-            a = Float.parseFloat(value);
-            firstNum = false;
-        }
-        else {  // else assign to b
-            b = Float.parseFloat(value);
-            firstNum = true;
-        }
-
         // concatenate the number to the current display
         result.setText(result.getText() + value);
+
+        lastKey = value;
     }
 
     @FXML
     public void processOps(ActionEvent e) {
+        Double output = 0.0;
+
         // store the text of the button pressed
         String value = ((Button)e.getSource()).getText();
 
-        if(!value.equals("=")) {
-            // if we pressed an operator, but there is already one stored, do nothing
-            if(!op.equals(""))
-                return;
-
-            // else store the pressed operator and display
-            op = value;
-            // a = Long.parseLong(result.getText());
+        if(!value.equals("="))
             result.setText(result.getText() + value);
-        }
 
-        // if = was pressed, perform operation
+        // if = was pressed, perform calculation
         else {
-            // if = was pressed, but there is no operator stored, do nothing
-            if(op.equals(""))
-                return;
+            // get full equation as string, then tokenize and try to parse
+            String equation = result.getText();
 
-            // otherwise, perform calculation and display result
-            float output = model.calculate(a, b, op);
-
-            // tricky way to display decimal only if needed
-            if(op.equals("/"))
-                result.setText(String.valueOf(output));
-            else {
-                int display = Math.round(output);   // since calc only allows integers as input, will only "chop" off .0
-                result.setText(String.valueOf(display));
+            try {
+                output = model.calculate(equation);
+            } catch (EmptyStackException exception) {
+                result.setText("ERROR");
+                clear();
             }
 
-            // reset values: no op, start is now true, and we are looking for firstNum
-            op = "";
-            start = true;
-            firstNum = true;
+            // tricky way to display decimal only if needed
+            if(output % 1 != 0)
+                result.setText(String.valueOf(output));
+            else {
+                int display = (int)Math.round(output);   // since calc only allows integers as input, will only "chop" off .0
+                result.setText(String.valueOf(display));
+            }
         }
+
+        lastKey = value;
     }
 }
--- src/core/Model.java
+++ src/core/Model.java
@@ -1,21 +1,125 @@
 package core;
 
+import java.util.EmptyStackException;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
 public class Model {
-    public float calculate(float a, float b, String op) {
+    private Stack<Double> operand = new Stack<>();
+    private Stack<String> operator = new Stack<>();
+
+    private void eval() {
+        // pop two operands and one operator, calculate, and push result onto operand stack
+        Double num1 = operand.pop();
+        Double num2 = operand.pop();
+        Double result = null;
+        String op = operator.pop();
+
         switch (op) {
             case "+":
-                return a + b;
+                result = num2 + num1;
+                break;
             case "-":
-                return a - b;
+                result = num2 - num1;
+                break;
             case "*":
-                return a * b;
+                result = num2 * num1;
+                break;
             case "/":
-                if(b == 0)
-                    return 0;
-                else
-                    return a / b;
+                result = num2 / num1;
+                break;
             default:
-                return 0;
+                throw new NumberFormatException();
         }
+        operand.push(result);
+    }
+
+    Double calculate(String eq) throws EmptyStackException {
+        StringTokenizer t = new StringTokenizer(eq, "+-*/()", true);
+        String nextToken;
+        Double num = null;
+        String currentOp = null;
+
+        // variables to allow for unary operators at the start of expression or after a left paren
+        boolean unaryOK = true;
+        double sign = 1;
+
+        // WHILE more tokens exist, get nextToken
+        while (t.hasMoreTokens()) {
+            nextToken = t.nextToken();
+            // SWITCH nextToken
+            switch (nextToken) {
+                // + or –: WHILE top of operator stack is '+', '-', '*', or '/'
+                case "+":
+                    if (unaryOK) {
+                        sign = 1;
+                        unaryOK = false;
+                        break;
+                    }
+                case "-":
+                    if (unaryOK) {
+                        sign = -1;
+                        unaryOK = false;
+                        break;
+                    }
+
+                    // if operator stack is empty, do not peak, just push operator
+                    if (operator.isEmpty())
+                        operator.push(nextToken);
+                    else {
+                        currentOp = operator.peek();
+                        while (currentOp.equals("+") || currentOp.equals("-") || currentOp.equals("*") || currentOp.equals("/")) {
+                            eval();
+
+                            if (!operator.isEmpty())
+                                currentOp = operator.peek();
+                            else
+                                currentOp = "";
+                        }
+                        operator.push(nextToken);
+                    }
+                    break;
+
+                // * or /:  WHILE top of operator() stack is '*' or '/'
+                case "*":
+                case "/":
+                    unaryOK = false;
+
+                    // if operator stack is empty, do not peak, just push operator
+                    if (operator.isEmpty())
+                        operator.push(nextToken);
+
+                    else {
+                        currentOp = operator.peek();
+                        while (currentOp.equals("*") || currentOp.equals("/")) {
+                            // CALL eval()
+                            eval();
+                            if (!operator.isEmpty())
+                                currentOp = operator.peek();
+                            else
+                                currentOp = "";
+                        }
+                        operator.push(nextToken);
+                    }
+                    break;
+
+                // DEFAULT: Convert to double & push nextToken to operand stack
+                default:
+                    unaryOK = false;
+                    num = Double.parseDouble(nextToken) * sign;
+                    operand.push(num);
+                    sign = 1;
+            }
+        }
+
+        // WHILE operator stack is not empty CALL eval()
+        while (!operator.isEmpty())
+            eval();
+
+        // Result is on top of operand stack
+        if (!operand.isEmpty())
+            return operand.peek();
+        else
+            throw new EmptyStackException();
     }
 }
